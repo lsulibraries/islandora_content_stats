9a7674606f20c8d4baf7ceae0dc9fb7123f6d164
Will <wwconlin@gmail.com>
2018-07-26 12:57:19 -0500

    swap form for template, working of passing vars through preprocess.

---
+++ theme/data.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for the object page for audio file
+ *
+ * @TODO: add documentation about file and available variables
+ */
+?>
+
+<?php dpm( $variables); ?>
+
+<div class="data-description"><?php print 'Hello World';?></div>
+
+
